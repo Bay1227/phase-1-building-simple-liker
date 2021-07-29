b02a6eedc44be20c4e95b9aa4308d7dada2cc291
Bayartsengel Battulga <tsengelbattulga1227@gmail.com>
2021-07-29 07:47:05 -0700

    commit tree 883d6569b231c383ae5ac35e9dbb9b75e1f036e9

--- main.js
+++ main.js
@@ -1,10 +1,41 @@
 // Defining text characters for the empty and full hearts for you to use later.
 const EMPTY_HEART = '♡'
 const FULL_HEART = '♥'
+FULL_HEART.class = `.activated-heart`
+console.log(FULL_HEART)
 
 // Your JavaScript code goes here!
+document.addEventListener("DOMContentLoaded", () => {
+  const hearts = document.querySelectorAll('span.like-glyph')
+  console.log(hearts)
+  
+  hearts.forEach(hearts => hearts.addEventListener('click', likeCallback))
+  modal.classList.add('hidden')
+  function likeCallback(hearts){
+    console.log(hearts.target)
+    mimicServerCall()
+    .then(() => {
+      if(hearts.target.innerHTML === EMPTY_HEART){
+        hearts.target.innerHTML = FULL_HEART
+      }
+      else if (hearts.target.innerHTML = FULL_HEART){
+        hearts.target.innerHTML === EMPTY_HEART
+        }
+      })
+      .catch(er => {
+        const erMsg = document.getElementById('modal-message')
+      //   console.log(erMsg)
+      //  erMsg.className = 'show'
+       modal.classList.remove('hidden')
+       erMsg.innerHTML = err
+
+       setTimeout(() => { erMsg.innerHTML = ""}, 3000)
+      })
+  }
 
 
+
+})
 
 
 //------------------------------------------------------------------------------
